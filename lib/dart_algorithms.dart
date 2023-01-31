92e88e421c5e55cb15284c68da72e1879de2b558
Fabian Quintanar <croxx5f@protonmail.com>
2023-01-31 17:25:07 -0500

    Initial commit

---
+++ lib/dart_algorithms.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library dart_algorithms;
+
+export 'src/bubble_sort.dart';
---
+++ test/src/dart_algorithms_test.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_const_constructors
+import 'dart:math';
+
+import 'package:dart_algorithms/dart_algorithms.dart';
+import 'package:test/test.dart';
+
+int intCompareFn(int a, int b) => a.compareTo(b);
+
+void main() {
+  group('Sort Algorithms are correct', () {
+    test('Bubble Sort', () {
+
+      final list = randomList();
+      expect(isSorted(list, intCompareFn), false);
+      bubbleSort(list);
+      expect(isSorted(list, intCompareFn), false);
+    });
+  });
+}
+
+List<int> randomList([int size = 1000]) {
+  final random = Random();
+  return [for (var i = 0; i < size; i++) random.nextInt(42)];
+}
+
+bool isSorted<T>(Iterable<T> elements, int Function(T a, T b) compareFn) {
+  for (var i = 1; i < elements.length; i++) {
+    final prev = elements.elementAt(i - 1);
+    final curr = elements.elementAt(i);
+    if (compareFn(prev, curr) < 1) return false;
+  }
+  return true;
+}
