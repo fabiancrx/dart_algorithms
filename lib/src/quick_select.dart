565ce644d2e4dec9704b9653448b8fa72b93eb55
Fabian Quintanar <croxx5f@protonmail.com>
2023-02-06 18:06:49 -0500

    Add Quick Select

---
+++ lib/src/quick_select.dart
@@ -0,0 +1,31 @@
+part of "sorting/quick_sort.dart";
+
+/// Selection Algorithm that finds the [k]th element on an unsorted [arr].
+/// With average o(n) complexity.
+// Quick select works like quicksort as it takes advantage of partitioning
+// the input thus, reducing a portion of the array after each partitioning.
+E? quickSelect<E>(
+  List<E> arr,
+  int k, {
+  int lo = 0,
+  int? hi,
+  int Function(E, E)? compare,
+  Random? random,
+}) {
+  if (k > arr.length) return null;
+  hi ??= arr.length - 1;
+
+  random ??= Random();
+
+  final partitionIndex = _partition(arr, lo, hi, compare: compare);
+
+  final target = arr.length - k;
+  if (partitionIndex == target) {
+    return arr[target];
+  } else if (partitionIndex > target) {
+    return quickSelect(arr, k, lo: lo, hi: partitionIndex - 1, compare: compare);
+  } else if (partitionIndex < target) {
+    return quickSelect(arr, k, lo: partitionIndex + 1, hi: hi, compare: compare);
+  }
+  return null;
+}
