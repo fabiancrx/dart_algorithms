ed39f9ab0db16e88e2b9ba8fdd26e9ceb2c71deb
Fabian Quintanar <croxx5f@protonmail.com>
2023-02-14 00:49:53 -0500

    test directed graphs

--- test/src/graph_traversal_test.dart
+++ test/src/graph_traversal_test.dart
@@ -7,21 +7,37 @@ import "fixtures/fixtures.dart";
 
 void main() {
   late final Graph<int> tinyG;
+  late final Graph<int> tinyDirectedG;
+  late final Graph<int> tinyDirectedG2;
   late final Graph<int> sample;
+
   setUpAll(() {
     tinyG = fromInput(loadTestFile("tinyG.txt"));
     sample = fromInput(loadTestFile("sample.txt"));
+    tinyDirectedG = fromInput(loadTestFile("tinyDG.txt"), directed: true);
+    tinyDirectedG2 = fromInput(loadTestFile("tinyDG2.txt"), directed: true);
   });
   group("Graph ", () {
+    test(" is correctly constructed when directed", () {
+      expect(tinyDirectedG.degree(7), 2);
+
+      expect(tinyDirectedG2.toString().isNotEmpty, true);
+
+      expect(tinyDirectedG.neighbours(7).contains(9), true);
+      expect(tinyDirectedG.neighbours(9).contains(7), false);
+      expect(dfs(tinyDirectedG, 7), unorderedEquals(tinyDirectedG.vertices));
+      expect(dfs(tinyDirectedG, 0), unorderedEquals([0, 1, 2, 3, 4, 5]));
+    });
+
     group("Traversal ", () {
       test("Depth First Search", () {
         expect(dfs(sample, 0), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
         expect(dfs(tinyG, 0), [0, 5, 4, 3, 6, 1, 2]);
-        print(tinyG);
       });
       test("Breadth First Search", () {
         expect(bfs(sample, 0), [0, 1, 4, 8, 2, 5, 7, 9, 3, 6]);
         expect(bfs(tinyG, 0), [0, 5, 1, 2, 6, 4, 3]);
+        expect(bfs(tinyDirectedG2, 0), [0, 1, 2, 4, 3, 5]);
       });
     });
 
@@ -52,10 +68,10 @@ void main() {
   });
 }
 
-Graph<int> fromInput(List<String> input) {
+Graph<int> fromInput(List<String> input, {bool directed = false}) {
   final vertices = int.parse(input.removeAt(0));
   final edges = input.removeAt(0);
-  final graph = AdjacencyListGraph(List.generate(vertices, (i) => i));
+  final graph = AdjacencyListGraph(List.generate(vertices, (i) => i), directed: directed);
 
   for (final edge in input) {
     final s = edge.split(" ");
