8f762fd7defd265ea8e9d90b20a6e6ee1dbb018d
Fabian Quintanar <croxx5f@protonmail.com>
2023-02-22 18:38:20 -0500

    test: sorting algorithms with different datasets

--- test/src/sorting_test.dart
+++ test/src/sorting_test.dart
@@ -4,15 +4,32 @@ import "package:dart_algorithms/dart_algorithms.dart";
 import "package:test/test.dart";
 
 int intCompareFn(int a, int b) => a.compareTo(b);
+
 int boxedIntCompareFn(BoxedInt a, BoxedInt b) => a.value.compareTo(b.value);
 
 typedef SortFunction = void Function<E>(
   List<E> elements, {
   int Function(E, E)? compare,
 });
-// TODO (croxx5f): Add stability and different data distributions and types in the tests:
-// sorted, partially sorted, reverse order, unsorted
+// TODO (croxx5f): Add stability
 void main() {
+  final datasets = [
+    randomList,
+    mostlySorted,
+    sortedList,
+    reverseOrderedList,
+  ];
+  final sortingAlgorithms = {
+    "Bubble Sort": bubbleSort,
+    "Insertion Sort": insertionSort,
+    "Selection Sort": selectionSort,
+    "Shell Sort": shellSort,
+    "Merge Sort": mergeSort,
+    "Merge Sort Iterative": mergeSortIterative,
+    "Quick Sort": quickSort,
+    "Quick Sort Simple implementation": quickSortSimple,
+    "Heap Sort": heapSort
+  };
   test("isSorted helper function", () {
     final list = randomList();
     expect(isSorted(list, intCompareFn), false);
@@ -21,60 +38,15 @@ void main() {
   });
 
   group("Sort Algorithms are correct", () {
-    test("Bubble Sort", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      bubbleSort(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
-    test("Selection Sort", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      selectionSort(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
-    test("Insertion Sort", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      insertionSort(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
-    test("Shell Sort", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      shellSort(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
-    test("Merge Sort", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      mergeSort(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
-    test("Merge Sort Iterative", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      mergeSortIterative(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
-    test("Quick Sort ", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      quickSort(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
-    test("Quick Sort Naive implementation", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      quickSortSimple(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
-    test("Heap Sort ", () {
-      final list = randomList();
-      expect(isSorted(list, intCompareFn), false);
-      heapSort(list);
-      expect(isSorted(list, intCompareFn), true);
-    });
+    for (final sort in sortingAlgorithms.entries) {
+      test(sort.key, () {
+        for (final dataset in datasets) {
+          final list = dataset();
+          sort.value(list);
+          expect(isSorted(list, intCompareFn), true);
+        }
+      });
+    }
   });
 }
 
@@ -101,7 +73,18 @@ List<int> randomList([int size = 1000]) {
   return [for (var i = 0; i < size; i++) random.nextInt(42)];
 }
 
+List<int> mostlySorted([int size = 1000]) {
+  final random = Random();
+  final list = sortedList(size);
+  for (var i = 0; i < size ~/ 10; i++) {
+    list.insert((i * 10) % size, random.nextInt(42));
+  }
+  return randomList(size)..sort();
+}
+
 List<int> sortedList([int size = 1000]) => randomList(size)..sort();
+
+List<int> reverseOrderedList([int size = 1000]) => sortedList(size).reversed.toList();
 
 bool isSorted<T>(Iterable<T> elements, int Function(T a, T b) compareFn) {
   for (var i = 1; i < elements.length; i++) {
